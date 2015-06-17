4aaed1522185294e5779873247edffa74f095201
Ramesh <rrameshp@gmail.com>
2015-06-17 22:06:20 +0530

    Added obtaining the distance in Km for difference in latitude and longitude.

--- TidyData.R
+++ TidyData.R
@@ -149,6 +149,24 @@ get_dist <- function(lon1, lat1, lon2, lat2) {
   return(d)
 }
 
+# Calculate distance in kilometers between two points
+# Appears similiar to the above function
+earth.dist <- function (long1, lat1, long2, lat2)
+{
+  rad <- pi/180
+  a1 <- lat1 * rad
+  a2 <- long1 * rad
+  b1 <- lat2 * rad
+  b2 <- long2 * rad
+  dlon <- b2 - a2
+  dlat <- b1 - a1
+  a <- (sin(dlat/2))^2 + cos(a1) * cos(b1) * (sin(dlon/2))^2
+  c <- 2 * atan2(sqrt(a), sqrt(1 - a))
+  R <- 6378.145
+  d <- R * c
+  return(d)
+}
+
 library(readr)
 library(rjson)
 
